b0dc4f751d7eccbbcfa83613386b9e1d4306f907
Noah Wagner <wagnernoah@icloud.com>
2021-05-01 18:05:50 -0700

    finished shape class

--- Geometry.js
+++ Geometry.js
@@ -14,21 +14,71 @@ class Point {
 	}
 }
 
-class Shape {
+class Shape extends Entity {
 	constructor(triangles = []) {
+		super();
 		this.triangles = triangles;
-		this.local = Matrix.identity(4);
+		this.toWorldSpace();
 	}
 
 	addTriangle(triangle) {
-
+		this.toLocalSpace();
+		this.tiangles.push(triangle);
+		this.toWorldSpace();
+		return this;
 	}
 
-	move(x, y = 0, z = 0) {
-		
+	move(moveVec) {
+		this.toLocalSpace();
+		super.move(moveVec);
+		this.toWorldSpace();
+		return this;
 	}
 
-	rotate(x, y = 0, z = 0) {
+	moveTo(translationVec) {
+		this.toLocalSpace();
+		super.moveTo(translationVec);
+		this.toWorldSpace();
+		return this;
+	}
 
+	rotate(rotX, rotY, rotZ) {
+		this.toLocalSpace();
+		super.rotate(rotX, rotY, rotZ);
+		this.toWorldSpace();
+		return this;
+	}
+
+	setRotation(rotX, rotY, rotZ) {
+		this.toLocalSpace();
+		super.setRotation(rotX, rotY, rotZ);
+		this.toWorldSpace();
+		return this;
+	}
+
+	toWorldSpace() {
+		for (let i = 0; i < this.triangles.length; i++) {
+			let triangle = this.triangles[i];
+			let world1 = Matrix.toArray(Matrix.multiply(this.localSpace, Matrix.fromArray([triangle.p1.x, triangle.p1.y, triangle.p1.z, 1])));
+			let world2 = Matrix.toArray(Matrix.multiply(this.localSpace, Matrix.fromArray([triangle.p2.x, triangle.p2.y, triangle.p2.z, 1])));
+			let world3 = Matrix.toArray(Matrix.multiply(this.localSpace, Matrix.fromArray([triangle.p3.x, triangle.p3.y, triangle.p3.z, 1])));
+			triangle.p1 = new Point(world1[0], world1[1], world1[2]);
+			triangle.p2 = new Point(world2[0], world2[1], world2[2]);
+			triangle.p3 = new Point(world3[0], world3[1], world3[2]);
+		}
+		return this;
+	}
+
+	toLocalSpace() {
+		for (let i = 0; i < this.triangles.length; i++) {
+			let triangle = this.triangles[i];
+			let world1 = Matrix.toArray(Matrix.multiply(Matrix.getInverseOf(this.localSpace), Matrix.fromArray([triangle.p1.x, triangle.p1.y, triangle.p1.z, 1])));
+			let world2 = Matrix.toArray(Matrix.multiply(Matrix.getInverseOf(this.localSpace), Matrix.fromArray([triangle.p2.x, triangle.p2.y, triangle.p2.z, 1])));
+			let world3 = Matrix.toArray(Matrix.multiply(Matrix.getInverseOf(this.localSpace), Matrix.fromArray([triangle.p3.x, triangle.p3.y, triangle.p3.z, 1])));
+			triangle.p1 = new Point(world1[0], world1[1], world1[2]);
+			triangle.p2 = new Point(world2[0], world2[1], world2[2]);
+			triangle.p3 = new Point(world3[0], world3[1], world3[2]);
+		}
+		return this;
 	}
 }
